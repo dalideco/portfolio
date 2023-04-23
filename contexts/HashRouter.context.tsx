350643a9910679fc7899e4caa3e9108ca37ae669
dalideco <dalissj1@gmail.com>
2023-04-24 00:08:28 +0100

    hash router context

--- contexts/HashRouter.context.tsx
+++ contexts/HashRouter.context.tsx
@@ -1,19 +1,43 @@
-import React, { createContext, useContext } from "react";
+import { useRouter } from "next/router";
+import React, { createContext, useContext, useEffect, useMemo } from "react";
 
-interface HashRouterContext {}
+interface HashRouterContext {
+  route: string | null;
+  ROUTES: string[];
+}
 
-const hashRouterContext = createContext<HashRouterContext>({});
+const hashRouterContext = createContext<HashRouterContext>({
+  route: null,
+  ROUTES: [],
+});
 
 export function useHashRouter() {
   return useContext(hashRouterContext);
 }
+
+const ROUTES = [
+  "about",
+  "experience",
+  "projects",
+  "education",
+  "skill",
+  "contact",
+];
 
 export function HashRouterProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const value = {};
+  const { asPath } = useRouter();
+
+  const route = useMemo(() => asPath.split("#")[1] || null, [asPath]);
+
+  useEffect(() => {
+    if (!route) return;
+  }, [route]);
+
+  const value = { route, ROUTES };
   return (
     <hashRouterContext.Provider value={value}>
       {children}
