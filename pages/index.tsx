3cddf9f7f4cc1474ca3cd76f85d87d39753382fc
dalideco <dalissj1@gmail.com>
2023-04-24 00:39:30 +0100

    padding hassh router provider to index route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,12 @@
 import Navbar from "@/components/Navbar";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { HashRouterProvider } from "@/contexts/HashRouter.context";
 
 export default function Home() {
-  const { asPath } = useRouter();
-
-  useEffect(() => {
-    const route = asPath.split("#")[1] || null;
-    console.log(route)
-  }, [asPath]);
-
   return (
-    <main>
-      <Navbar />
-    </main>
+    <HashRouterProvider>
+      <main>
+        <Navbar />
+      </main>
+    </HashRouterProvider>
   );
 }
