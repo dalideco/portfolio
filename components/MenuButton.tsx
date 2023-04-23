e2033d85f888958bd226b7cd6180a8fbed0684ca
dalideco <dalissj1@gmail.com>
2023-04-24 00:38:03 +0100

    creating menu button

---
+++ components/MenuButton.tsx
@@ -0,0 +1,10 @@
+import styles from "@/styles/components/MenuButton.module.css";
+
+export default function MenuButton() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.first}></div>
+      <div className={styles.second}></div>
+    </div>
+  );
+}
