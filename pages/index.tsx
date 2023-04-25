8b91d2ffeef62a73d9699611f261bc5a93e731b6
dalideco <dalissj1@gmail.com>
2023-04-25 01:43:27 +0100

    providing mouse

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import Navbar from "@/components/Navbar";
 import { HashRouterProvider } from "@/contexts/HashRouter.context";
+import { MouseProvider } from "@/contexts/Mouse.context";
 
 export default function Home() {
   return (
     <HashRouterProvider>
-      <main>
-        <Navbar />
-      </main>
+      <MouseProvider>
+        <main>
+          <Navbar />
+        </main>
+
+        <div style={{height: "200vh"}}></div>
+      </MouseProvider>
     </HashRouterProvider>
   );
 }
