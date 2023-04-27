624b483e32148094c61ce8bd59c94aa303903605
dalideco <dalissj1@gmail.com>
2023-04-27 03:41:36 +0100

    adding about

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import { HashRouterProvider } from "@/contexts/HashRouter.context";
 import { MouseProvider } from "@/contexts/Mouse.context";
+import About from "@/parts/About";
 
 export default function Home() {
   return (
@@ -8,9 +9,10 @@ export default function Home() {
       <MouseProvider>
         <main>
           <Navbar />
+          <About />
         </main>
 
-        <div style={{height: "200vh"}}></div>
+        <div style={{ height: "200vh" }}></div>
       </MouseProvider>
     </HashRouterProvider>
   );
---
+++ parts/About.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "@/styles/parts/About.module.css";
+
+export default function About() {
+  return (
+    <div id="about">
+      <h1 className={styles.title}>Make a difference with your software</h1>
+    </div>
+  );
+}
