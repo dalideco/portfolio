c8bb017e912994ca2ebb93692b8fa01584492252
dalideco <dalissj1@gmail.com>
2023-07-27 19:48:24 +0200

    creating rectangle animation for about me

--- parts/About.tsx
+++ parts/About.tsx
@@ -6,29 +6,47 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 export default function About() {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
-    const test = gsap.to("#title", {
+    const titleAnimation = gsap.to("#title", {
       scrollTrigger: {
         trigger: "#title",
         toggleActions: "restart none none reverse",
         start: "bottom 40%",
-        markers: true,
+        markers: false,
       },
 
       scale: 0.7,
       y: -100,
       duration: 2,
     });
+
+    const aboutmeAnimation = gsap.to("#about-me-container", {
+      scrollTrigger: {
+        trigger: "#title",
+        toggleActions: "restart none none reverse",
+        start: "bottom 40%",
+        markers: false,
+      },
+
+      y: -120,
+      duration: 1,
+    });
+
     return () => {
-      test.kill();
+      titleAnimation.kill();
+      aboutmeAnimation.kill();
     };
   }, []);
 
   return (
     <div>
       {/* <div style={{ height: "100vh" }}></div> */}
+      {/* Title */}
       <h1 id="title" className={styles.title}>
         Make a difference with your software
       </h1>
+
+      {/* Information rectangle */}
+      <div id="about-me-container" className={styles.aboutmeContainer}></div>
     </div>
   );
 }
