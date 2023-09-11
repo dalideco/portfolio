97a867858faac7171cf04d5069786805ad42abaa
dalideco <dalissj1@gmail.com>
2023-09-11 21:47:33 +0200

    image name & email

--- parts/About.tsx
+++ parts/About.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import styles from "@/styles/parts/About.module.css";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import Image from "next/image";
 
 export default function About() {
   useEffect(() => {
@@ -9,7 +10,7 @@ export default function About() {
     const titleAnimation = gsap.to("#title", {
       scrollTrigger: {
         trigger: "#title",
-        toggleActions: "restart none none reverse",
+        scrub: true,
         start: "bottom 40%",
         markers: false,
       },
@@ -22,12 +23,33 @@ export default function About() {
     const aboutmeAnimation = gsap.to("#about-me-container", {
       scrollTrigger: {
         trigger: "#title",
-        toggleActions: "restart none none reverse",
+        scrub: true,
         start: "bottom 40%",
         markers: false,
       },
+      y: -50,
+      duration: 1,
+    });
 
-      y: -120,
+    const imageAnimation = gsap.to("#about-me-container img", {
+      scrollTrigger: {
+        trigger: "#title",
+        scrub: true,
+        start: "bottom 40%",
+        markers: false,
+      },
+      scale: 0.9,
+      duration: 0.5,
+    });
+
+    const subtitleAnimation = gsap.to("#about-me-container #subtitle", {
+      scrollTrigger: {
+        trigger: "#about-me-container",
+        scrub: true,
+        start: "top 40%",
+        markers: false,
+      },
+      x: 30,
       duration: 1,
     });
 
@@ -46,9 +68,29 @@ export default function About() {
       </h1>
 
       {/* Information rectangle */}
-      <div id="about" className={styles.aboutmeContainer}>
+      <div
+        id="about-me-container"
+        className="h-[70vh] mx-6 bg-lightgray flex overflow-hidden"
+      >
+        <div className="flex-1 flex flex-col justify-center p-8">
+          <h2> Mohamed Ali Sahnoun </h2>
+          <p id="subtitle" className="text-gray ml-2 mt-2">
+            {" "}
+            Based in Tunisia{" "}
+          </p>
+        </div>
 
+        <Image
+          className="flex-1 object-cover scale-110"
+          src="/photos/me.jpeg"
+          width={200}
+          height={200}
+          alt="me"
+        />
 
+        <div className="flex-1 flex flex-col justify-center p-8">
+          <h3> sahnoun.mohamedali1@gmail.com </h3>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,14 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './parts/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     colors: {
       black: "#1D1D1D",
       white: "#F5F5F5",
       gray: "#666666",
-      lightgray: "E7E7E7"
+      lightgray: "#E7E7E7"
     }
   },
   plugins: [],
