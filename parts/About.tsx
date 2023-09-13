bc2baf6fabfc79a57be3c3bc8d1342fef866b24b
dalideco <dalissj1@gmail.com>
2023-09-13 10:57:07 +0200

    setting animations as separate functions

--- parts/About.tsx
+++ parts/About.tsx
@@ -5,22 +5,22 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import Image from "next/image";
 
 export default function About() {
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    const titleAnimation = gsap.to("#title", {
+  const animateTitle = () => {
+    return gsap.to("#title", {
       scrollTrigger: {
         trigger: "#title",
-        scrub: true,
+        toggleActions: "restart none none reverse",
         start: "bottom 40%",
         markers: false,
       },
-
       scale: 0.7,
       y: -100,
       duration: 2,
     });
+  };
 
-    const aboutmeAnimation = gsap.to("#about-me-container", {
+  const animateAboutMeContainer = () => {
+    return gsap.to("#about-me-container", {
       scrollTrigger: {
         trigger: "#title",
         scrub: true,
@@ -30,8 +30,10 @@ export default function About() {
       y: -50,
       duration: 1,
     });
+  };
 
-    const imageAnimation = gsap.to("#about-me-container img", {
+  const animateImage = () => {
+    return gsap.to("#about-me-container img", {
       scrollTrigger: {
         trigger: "#title",
         scrub: true,
@@ -41,8 +43,10 @@ export default function About() {
       scale: 0.9,
       duration: 0.5,
     });
+  };
 
-    const subtitleAnimation = gsap.to("#about-me-container #subtitle", {
+  const animateSubtitle = () => {
+    return gsap.to("#about-me-container #subtitle", {
       scrollTrigger: {
         trigger: "#about-me-container",
         scrub: true,
@@ -52,6 +56,15 @@ export default function About() {
       x: 30,
       duration: 1,
     });
+  };
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const titleAnimation = animateTitle();
+    const aboutmeAnimation = animateAboutMeContainer();
+    const imageAnimation = animateImage();
+    const subtitleAnimation = animateSubtitle();
 
     return () => {
       titleAnimation.kill();
@@ -72,7 +85,7 @@ export default function About() {
       {/* Information rectangle */}
       <div
         id="about-me-container"
-        className="h-[70vh] mx-6 bg-lightgray flex overflow-hidden"
+        className="h-[70vh] mx-6 bg-lightgray flex overflow-hidden flex-col lg:flex-row items-center lg:items-stretch"
       >
         <div className="flex-1 flex flex-col justify-center p-8">
           <h2> Mohamed Ali Sahnoun </h2>
